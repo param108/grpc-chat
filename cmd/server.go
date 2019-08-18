e61b12c961d78ac5478be4097b1ad3622d3b7073
Paramananda.P <param.ponnaiyan@gmail.com>
2019-08-18 18:08:33 +0530

    [paramananda.p] Add code to setup the PID for better deploy

--- cmd/server.go
+++ cmd/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/param108/grpc-chat/chat/chatserver"
 	"github.com/spf13/cobra"
+	"io/ioutil"
 	"os"
 	"strconv"
 )
@@ -14,16 +15,31 @@ var ServerCmd = &cobra.Command{
 	RunE:  serverCmdF,
 }
 
+func createPIDFile() error {
+	pid := os.Getpid()
+	err := ioutil.WriteFile("PID", []byte(strconv.Itoa(pid)), 0644)
+	return err
+}
+
 func serverCmdF(cmd *cobra.Command, args []string) error {
 	port, err := strconv.Atoi(os.Getenv("SERVER_PORT"))
 	if err != nil {
 		fmt.Printf("Failed to identify server port: %s\n", err.Error())
+		return err
+	}
+
+	err = createPIDFile()
+	if err != nil {
+		fmt.Printf("Failed to open PID file %s", err.Error())
+		return err
 	}
 	server, err := chatserver.NewChatServer(port)
+
 	if err != nil {
 		fmt.Printf("Failed to create new Server %s\n", err.Error())
 		return err
 	}
 	server.Start()
+
 	return nil
 }
